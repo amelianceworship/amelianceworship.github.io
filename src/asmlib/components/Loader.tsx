65e09804a4b66d08e65beeffb2a476993f132941
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-18 16:56:10 +0200

    [+]: add asmlib

---
+++ src/asmlib/components/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Loader() {
+  return (
+    <div className="zero-block">
+      <div className="blackout show" />
+      <div className="page-loader">
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+}
