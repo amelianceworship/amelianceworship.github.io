d2630f4f358c65fca262a5d453d130093acd245d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-18 17:01:26 +0200

    [+]: add start layouts

---
+++ src/App/pages/ChordsList/ChordsList.tsx
@@ -0,0 +1,9 @@
+export function ChordsList() {
+	return (
+		<main className="main">
+			<h1 className="h1">
+				ChordsList
+			</h1>
+		</main>
+	);
+}
---
+++ src/App/pages/SignUp.tsx
@@ -0,0 +1,9 @@
+export function SignUp() {
+	return (
+		<main className="main">
+			<h1 className="h1">
+				Sign Up
+			</h1>
+		</main>
+	);
+}
