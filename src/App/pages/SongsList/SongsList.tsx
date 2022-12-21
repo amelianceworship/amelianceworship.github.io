49abc197c614336f5417214c45381db2ad406312
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-21 20:34:26 +0200

    [^]: add groupe to SongsList

--- src/App/pages/SongsList/SongsList.tsx
+++ src/App/pages/SongsList/SongsList.tsx
@@ -6,10 +6,13 @@ import { useTypedDispatch } from '~store/hooks/useTypedDispatch';
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
 import { fetchSongsList } from '~store/songsList/actions/fetchSongsList';
 
+import './SongsList.scss';
+
 export function SongsList() {
 	const {
 		error, isLoading, songsList, songsTableNames,
 	} = useTypedSelector((state) => state.songsListReducer);
+
 	const [activeTable, setActiveTable] = useState(songsTableNames[0]);
 
 	const dispatch = useTypedDispatch();
@@ -23,7 +26,7 @@ export function SongsList() {
 	};
 
 	return (
-		<main className="main">
+		<main className="main songs-list-page">
 			<section className="container">
 				{isLoading && <h1 className="h1">loading...</h1>}
 				{songsList[activeTable] && songsList[activeTable].length > 0
@@ -35,7 +38,18 @@ export function SongsList() {
 						/>
 					)}
 				{ songsList[activeTable] && songsList[activeTable].length > 0
-					&& songsList[activeTable].map((songName) => <p className="p1" key={songName}>{songName}</p>)}
+					&& (
+						<div className="songs-list">
+							{songsList[activeTable].map((songGroup) => (
+								<div className="songs-list__group songs-group" key={songGroup[0]}>
+									<h4 className="h4 songs-group__title">{songGroup[0]}</h4>
+									<div className="songs">
+										{songGroup[1].map((songName) => <p className="p1 songs__item" key={songName}>{songName}</p>)}
+									</div>
+								</div>
+							))}
+						</div>
+					)}
 			</section>
 		</main>
 	);
