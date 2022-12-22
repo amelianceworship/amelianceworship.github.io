5bd5cc97ebd1dec1b0425f1b1cf8bac864b4dad0
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-22 22:42:56 +0200

    = add Modal component

---
+++ src/App/components/Modal.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import asm from 'asm-ts-scripts';
+
+import { Backdrop } from './Backdrop';
+import { Portal } from './Portal';
+
+import './Modal.scss';
+
+interface IModalProps {
+  children: React.ReactElement;
+  className?: string;
+  onClose: () => void;
+}
+
+export function Modal({ children, className, onClose }: IModalProps) {
+	const [show, setShow] = useState('show');
+
+	const backdropClickHandler = () => {
+		setShow('');
+	};
+
+	const handleAnimationend = () => {
+		if (show !== 'show') {
+			onClose();
+			document.body.style.overflow = 'visible';
+		}
+	};
+
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+	}, []);
+
+	const modalClass = asm.joinClasses('modal', className, show);
+
+	return (
+		<Portal>
+			<div className={modalClass} onAnimationEnd={handleAnimationend}>
+				<Backdrop onClick={backdropClickHandler} />
+				<div className="modal__content">{children}</div>
+			</div>
+		</Portal>
+	);
+}
