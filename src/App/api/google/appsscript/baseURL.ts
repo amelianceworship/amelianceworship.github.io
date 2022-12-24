f6b7aacec4c73cf01dc57bfed550a6b01ea48e16
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-24 21:42:32 +0200

    * update constant name

--- src/App/api/google/appsscript/baseURL.ts
+++ src/App/api/google/appsscript/baseURL.ts
@@ -1,1 +1,1 @@
-export const baseURL = `https://script.google.com/macros/s/${import.meta.env.VITE_GOOGLE_SCRIPT_ID_ANALITYCS}/exec`;
+export const baseURL = `https://script.google.com/macros/s/${import.meta.env.VITE_GOOGLE_APPSSCRIPT_ID_ANALITYCS}/exec`;
