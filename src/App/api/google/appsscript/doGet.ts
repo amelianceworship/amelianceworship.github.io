71ae3f36a78f59eb1e5d56dfa90a771f8eb09307
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-12-26 22:36:22 +0200

    * update google apps script api

--- src/App/api/google/appsscript/doGet.ts
+++ src/App/api/google/appsscript/doGet.ts
@@ -1,10 +1,56 @@
 import { baseURL } from './baseURL';
 
-export async function doGet() {
-	return fetch(baseURL)
+interface DoGet {
+	spreadsheetId: string;
+	neededSheet?: string;
+	columns?: string[];
+	action?: string;
+}
+
+export interface Response {
+	status: 'success' | 'partially' | 'error';
+	data: {
+		[key: string]: {
+			colNumber: number;
+			values: {
+				position: string;
+				value: string;
+			}[];
+		};
+	};
+	info: {
+		spreadsheetId: string;
+		neededSheet?: string;
+		action?: string;
+		found?: string[];
+		headings?: string[];
+		missed?: string[];
+		length?: string[];
+		columns?: string[];
+	};
+	error?: string;
+}
+
+export async function doGet({
+	spreadsheetId, neededSheet, columns, action,
+}: DoGet) {
+	const URLParams = new URLSearchParams();
+	if (spreadsheetId) URLParams.append('spreadsheetId', spreadsheetId);
+	if (neededSheet) URLParams.append('neededSheet', neededSheet);
+	if (columns) URLParams.append('columns', JSON.stringify(columns));
+	if (action) URLParams.append('action', action);
+
+	return fetch(`${baseURL}?${URLParams}`)
 		.then((response) => response.text())
 		.then((textData) => JSON.parse(textData))
-		.then((data) => data)
+		.then((data: Response) => {
+			if (data.status !== 'success') {
+				console.log(data.status);
+				console.log(data.info);
+				console.log(data.error);
+			}
+			return data;
+		})
 		// eslint-disable-next-line no-console
 		.catch((error) => console.log('setData', error));
 }
--- src/App/api/google/appsscript/doPost.ts
+++ src/App/api/google/appsscript/doPost.ts
@@ -1,23 +1,56 @@
 import { baseURL } from './baseURL';
 
 interface DoPost {
-	listName: string;
+	spreadsheetId: string;
+	neededSheet?: string;
 	dataParams: Record<string, unknown>;
 	action?: string;
 }
 
-export async function doPost({ listName, dataParams, action }: DoPost) {
+export interface Response {
+	status: 'success' | 'partially' | 'error';
+	data: {
+		[key: string]: {
+			colNumber: number;
+			values: {
+				position: string;
+				value: string;
+			}[];
+		};
+	};
+	info: {
+		spreadsheetId: string;
+		neededSheet?: string;
+		dataParams: Record<string, unknown>;
+		action?: string;
+		found?: string[];
+		headings?: string[];
+		missed?: string[];
+		length?: string[];
+	};
+	error?: string;
+}
+
+export async function doPost({
+	spreadsheetId, neededSheet, dataParams, action,
+}: DoPost) {
 	const URLParams = new URLSearchParams();
-	if (listName) URLParams.append('listName', listName);
+	if (spreadsheetId) URLParams.append('spreadsheetId', spreadsheetId);
+	if (neededSheet) URLParams.append('neededSheet', neededSheet);
 	if (dataParams) URLParams.append('dataParams', JSON.stringify(dataParams));
 	if (action) URLParams.append('action', action);
 
-	return fetch(`${baseURL}?${URLParams}`, {
-		method: 'POST',
-	})
+	return fetch(`${baseURL}?${URLParams}`, { method: 'POST' })
 		.then((response) => response.text())
 		.then((textData) => JSON.parse(textData))
-		.then((data) => data)
+		.then((data: Response) => {
+			if (data.status !== 'success') {
+				console.log(data.status);
+				console.log(data.info);
+				console.log(data.error);
+			}
+			return data;
+		})
 		// eslint-disable-next-line no-console
 		.catch((error) => console.log('setData', error));
 }
