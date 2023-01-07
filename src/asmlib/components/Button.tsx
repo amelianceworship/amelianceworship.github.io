95ae062d8d6e60d86ef88c9264e30bdf1ff7bfcb
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-07 18:27:32 +0200

    ^ update asmlib

--- src/asmlib/components/Button.tsx
+++ src/asmlib/components/Button.tsx
@@ -12,36 +12,36 @@ interface IButton {
 }
 
 export function Button({
-  children,
-  callback,
-  iconPosition,
-  icon,
-  id,
-  buttonClass = 'button',
-  type = 'primary',
-  disabled = false,
+	children,
+	callback,
+	iconPosition,
+	icon,
+	id,
+	buttonClass = 'button',
+	type = 'primary',
+	disabled = false,
 }: IButton) {
-  let iconClassName = '';
-  if (buttonClass === 'button-icon' || buttonClass === 'button-icon-sm') {
-    iconClassName = `icon center ${icon}`;
-  }
-  const buttonClassAndType = `${buttonClass} ${type}`;
+	let iconClassName = '';
+	if (buttonClass === 'button-icon' || buttonClass === 'button-icon-sm') {
+		iconClassName = `icon center ${icon}`;
+	}
+	const buttonClassAndType = `${buttonClass} ${type}`;
 
-  return (
-    <button
-      type="button"
-      id={id}
-      className={buttonClassAndType}
-      onClick={callback}
-      disabled={disabled}
-    >
-      {iconClassName.length === 0 && icon && iconPosition === 'left' && (
-        <span className={`icon left ${icon}`} />
-      )}
-      {icon ? <span className={iconClassName} /> : <span className="label">{children}</span>}
-      {iconClassName.length === 0 && icon && iconPosition === 'right' && (
-        <span className={`icon right ${icon}`} />
-      )}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			id={id}
+			className={buttonClassAndType}
+			onClick={callback}
+			disabled={disabled}
+		>
+			{iconClassName.length === 0 && icon && iconPosition === 'left' && (
+				<span className={`icon left ${icon}`} />
+			)}
+			{icon ? <span className={iconClassName} /> : <span className="label">{children}</span>}
+			{iconClassName.length === 0 && icon && iconPosition === 'right' && (
+				<span className={`icon right ${icon}`} />
+			)}
+		</button>
+	);
 }
