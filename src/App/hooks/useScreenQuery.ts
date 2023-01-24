3f1cb141b5f8f5d05f8704294fcdefa8b7868bf2
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 01:46:19 +0200

    * refactor useScreenQuery code

--- src/App/hooks/useScreenQuery.ts
+++ src/App/hooks/useScreenQuery.ts
@@ -3,18 +3,12 @@ import { useMediaQuery } from 'react-responsive';
 import { SCREEN_SIZES } from '~constants/SCREEN_SIZES';
 
 export function useScreenQuery() {
-	const isScreenXX = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XX}px)` });
-	const isScreenXL = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XL}px)` });
-	const isScreenLG = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_LG}px)` });
-	const isScreenMD = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_MD}px)` });
-	const isScreenXS = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XS}px)` });
-	const isScreenSS = useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_SS}px)` });
 	return {
-		isScreenXX,
-		isScreenXL,
-		isScreenLG,
-		isScreenMD,
-		isScreenXS,
-		isScreenSS,
+		isScreenXX: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XX}px)` }),
+		isScreenXL: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XL}px)` }),
+		isScreenLG: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_LG}px)` }),
+		isScreenMD: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_MD}px)` }),
+		isScreenXS: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XS}px)` }),
+		isScreenSS: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_SS}px)` }),
 	};
 }
