5095ed58fab52b1cabd324e081e9b3b8947f65fa
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 01:45:16 +0200

    ^ update useAuth hook

--- src/App/hooks/useAuth.ts
+++ src/App/hooks/useAuth.ts
@@ -1,8 +1,8 @@
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
 
 export function useAuth() {
-	const { uid } = useTypedSelector((state) => state.userReducer);
+	const { displayName } = useTypedSelector((state) => state.userReducer);
 	return {
-		isAuth: !!uid,
+		isAuth: !!displayName,
 	};
 }
