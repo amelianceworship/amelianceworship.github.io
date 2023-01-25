696f8e6e00a6465d90d6b811fa5fff829daaafa3
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 02:35:15 +0200

    + add songslist navigation

--- src/App/pages/SongsList/SongsList.tsx
+++ src/App/pages/SongsList/SongsList.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import asm from 'asm-ts-scripts';
 
 import { Dropdown } from '~components/inputs/Dropdown';
-import { Loader } from '~components/Loader';
+import { LoaderOverlay } from '~components/LoaderOverlay';
 import { useTypedDispatch } from '~store/hooks/useTypedDispatch';
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
 import { fetchSongsList } from '~store/songsList/actions/fetchSongsList';
@@ -12,7 +12,7 @@ import s from './SongsList.module.scss';
 
 export function SongsList() {
 	const {
-		error, isLoading, songsList, songsTableNames,
+		isLoading, songsList, songsTableNames,
 	} = useTypedSelector((state) => state.songsListReducer);
 
 	const [activeTable, setActiveTable] = useState(songsTableNames[0]);
@@ -20,18 +20,21 @@ export function SongsList() {
 	const dispatch = useTypedDispatch();
 
 	useEffect(() => {
-		dispatch(fetchSongsList());
-	}, [dispatch]);
+		if (asm.isObjectEmpty(songsList)) {
+			dispatch(fetchSongsList());
+		}
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	const handleTableNameChange = (select: string) => {
 		setActiveTable(select);
 	};
 
 	return (
-		<main className={asm.joinClasses(s.SongsList, 'main')}>
+		<main className={asm.joinClasses(s.SongsList, 'main', 'songsList')}>
 			<section className={asm.joinClasses(s.container, 'container')}>
-				{isLoading && <Loader />}
-				{songsList[activeTable] && songsList[activeTable].length > 0
+				{isLoading && <LoaderOverlay />}
+				{ songsList[activeTable].length > 0
 					&& (
 						<Dropdown
 							selected={songsTableNames[0]}
@@ -40,18 +43,26 @@ export function SongsList() {
 						/>
 					)}
 				{ songsList[activeTable] && songsList[activeTable].length > 0
-					&& (
-						<div className={s.songsList}>
-							{songsList[activeTable].map((songGroup) => (
-								<div className={s.songsGroup} key={songGroup[0]}>
-									<h3 className={asm.joinClasses(s.songsGroupSymbol, 'h3')}>{songGroup[0]}</h3>
-									<div className={s.songs}>
-										{songGroup[1].map((songName) => <p className="p1 songs__item" id={`song_${songName.position}`} key={songName.position}>{songName.value}</p>)}
-									</div>
-								</div>
-							))}
-						</div>
-					)}
+				&&	(
+					<nav className={s.listNavigation}>
+						{songsList[activeTable].map((songGroup) => (
+							<a href={`#${songGroup[0]}`} className="link" key={songGroup[0]}>{songGroup[0]}</a>
+						))}
+					</nav>
+				)}
+				{ songsList[activeTable] && songsList[activeTable].length > 0
+				&& (
+					<div className={s.songsList}>
+						{songsList[activeTable].map((songGroup) => (
+							<div className={s.songsGroup} key={songGroup[0]}>
+								<h3 className={asm.joinClasses(s.songsGroupSymbol, 'h3')} id={songGroup[0]}>{songGroup[0]}</h3>
+								<ul className={s.songs}>
+									{songGroup[1].map((songName) => <li className="p1 songs__item" id={`song_${songName.position}`} key={songName.position}>{songName.value}</li>)}
+								</ul>
+							</div>
+						))}
+					</div>
+				)}
 			</section>
 		</main>
 	);
