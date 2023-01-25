972019df780d9c38a56b2d46fd5645687b489c8b
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 02:00:08 +0200

    ^ update Icon component - add disabled

--- src/App/components/Icon.tsx
+++ src/App/components/Icon.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import asm from 'asm-ts-scripts';
 
 import s from './Icon.module.scss';
@@ -5,17 +7,21 @@ import s from './Icon.module.scss';
 interface Params {
 	icon: string;
 	size?: 'normal' | 'small';
-	isClickable?: boolean;
-	onclick?: () => void;
+	clickable?: boolean;
+	onclick?: (event: React.MouseEvent<HTMLElement>) => void;
+	disabled?: boolean;
 }
 
 export function Icon({
-	icon, size = 'normal', isClickable, onclick,
+	icon, size = 'normal', clickable, onclick, disabled,
 }: Params) {
 	const iconSize = size === 'small' ? 'icon-sm' : 'icon';
-	const iconClickable = isClickable ? 'click' : '';
+	const iconClickable = clickable && 'click';
 	return (
 		// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-		<span className={asm.joinClasses(s.Icon, icon, iconSize, iconClickable)} onClick={onclick} />
+		<span
+			className={asm.joinClasses(s.Icon, icon, iconSize, iconClickable, disabled && s.disabled)}
+			onClick={onclick}
+		/>
 	);
 }
