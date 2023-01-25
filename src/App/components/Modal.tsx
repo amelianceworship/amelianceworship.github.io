b430023d5c174ee21dbba9eeeb830af9048c6832
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 02:06:06 +0200

    = implement Modal

--- src/App/components/Modal.tsx
+++ src/App/components/Modal.tsx
@@ -2,33 +2,90 @@ import { useEffect, useState } from 'react';
 
 import asm from 'asm-ts-scripts';
 
+import { useScrollLock } from '~hooks/useScrollLock';
+
 import { Backdrop } from './Backdrop';
+import { Button } from './inputs/Button';
 import s from './Modal.module.scss';
 import { Portal } from './Portal';
 
-interface IModalProps {
-	children: React.ReactElement;
-	className?: string;
-	onClose: () => void;
+interface Button {
+	text?: string;
+	onClick?: () => void;
+	icon?: string;
+	iconPosition?: 'left' | 'right';
+	isDisabled?: boolean;
+	isButtonIcon?: boolean;
+	size?: 'normal' | 'small';
+	isSubmit?: boolean;
+	type?: 'primary' | 'secondary';
+	form?: string;
 }
 
-export function Modal({ children, className, onClose }: IModalProps) {
+interface ModalProps {
+	type?: 'alert' | 'info' | 'success' | 'error' | 'warn';
+	heading?: string;
+	children: React.ReactElement;
+	className?: string;
+	mainButton?: Button;
+	secondButton?: Button;
+	backdrop?: {
+		onClick?: { (): void } | null;
+		isDisabled?: boolean;
+	};
+	onClose?: () => void;
+	isButtons?: boolean;
+	size?: 'flex' | 'medium' | 'large';
+}
+
+export function Modal({
+	type,
+	heading,
+	children,
+	className,
+	mainButton,
+	secondButton,
+	backdrop,
+	onClose,
+	isButtons = true,
+	size = 'flex',
+}: ModalProps) {
 	const [show, setShow] = useState('show');
 
-	const backdropClickHandler = () => {
+	const isSecondButton = secondButton && !asm.isObjectEmpty(secondButton);
+
+	const closeModal = () => {
 		setShow('');
 	};
 
 	const handleAnimationend = () => {
 		if (show !== 'show') {
-			onClose();
+			closeModal();
 			document.body.style.overflow = 'visible';
+			if (onClose) onClose();
 		}
+	};
+
+	const backdropClickHandler = () => {
+		if (backdrop?.onClick) backdrop.onClick();
+		closeModal();
+	};
+
+	const mainButtonHandler = () => {
+		if (mainButton?.onClick) mainButton.onClick();
+		if (!mainButton?.form) closeModal();
+	};
+
+	const secondButtonHandler = () => {
+		if (secondButton?.onClick) secondButton.onClick();
+		if (!secondButton?.form) closeModal();
 	};
 
 	useEffect(() => {
 		document.body.style.overflow = 'hidden';
 	}, []);
+
+	const sizeClass = (size === 'medium' && s.medium) || (size === 'large' && s.large);
 
 	return (
 		<Portal>
@@ -36,8 +93,56 @@ export function Modal({ children, className, onClose }: IModalProps) {
 				className={asm.joinClasses(s.Modal, className, show)}
 				onAnimationEnd={handleAnimationend}
 			>
-				<Backdrop onClick={backdropClickHandler} />
-				<div className={s.content}>{children}</div>
+				<Backdrop onClick={backdropClickHandler} disabled={backdrop?.isDisabled} />
+				<div className={asm.joinClasses(sizeClass, s.content)}>
+					{(heading || type) && (
+						// <div className={type && asm.joinClasses(s.heading, type)}>
+						<h4 className={asm.joinClasses(type && s.heading, type && type, 'h4')}>
+							{!heading && type === 'alert' && 'СПОВІЩЕННЯ!'}
+							{!heading && type === 'error' && 'ПОМИЛКА!'}
+							{!heading && type === 'warn' && 'УВАГА!'}
+							{!heading && type === 'info' && 'ІНФОРМАЦІЯ!'}
+							{!heading && type === 'success' && 'УСПІХ!'}
+							{heading && heading}
+						</h4>
+						// </div>
+					)}
+					<div className={s.body}>
+						{children}
+					</div>
+					{isButtons && (
+						<div className={s.buttons}>
+							{isSecondButton && (
+								<Button
+									size={secondButton?.size}
+									icon={secondButton?.icon}
+									iconPosition={secondButton?.iconPosition}
+									type={secondButton?.type || 'secondary'}
+									onClick={() => secondButtonHandler()}
+									isButtonIcon={secondButton?.isButtonIcon}
+									form={secondButton?.form}
+									isSubmit={secondButton?.isSubmit}
+									disabled={secondButton?.isDisabled}
+								>
+									{secondButton?.text || 'Відміна'}
+								</Button>
+							)}
+							<Button
+								size={mainButton?.size}
+								type={mainButton?.type || 'primary'}
+								icon={mainButton?.icon}
+								iconPosition={mainButton?.iconPosition}
+								onClick={() => mainButtonHandler()}
+								isButtonIcon={mainButton?.isButtonIcon}
+								form={mainButton?.form}
+								isSubmit={mainButton?.isSubmit}
+								disabled={mainButton?.isDisabled}
+							>
+								{mainButton?.text || 'Ок'}
+							</Button>
+						</div>
+					)}
+				</div>
 			</div>
 		</Portal>
 	);
