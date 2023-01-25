f55c0a20a83aee25face7b40203adb4d10cd7bad
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 02:07:14 +0200

    = add Avatar component

---
+++ src/App/components/Avatar.tsx
@@ -0,0 +1,46 @@
+import asm from 'asm-ts-scripts';
+
+import s from './Avatar.module.scss';
+
+interface Props {
+	src?: string;
+	alt?: string;
+	char?: string;
+	color?: string;
+	size?: 'normal' | 'small';
+	isClickable?: boolean;
+	onClick?: () => void;
+	icon?: string;
+}
+
+export function Avatar({
+	src, alt, char, color, size = 'normal', isClickable = false, onClick, icon,
+}: Props) {
+	const smallClass = size === 'small' && s.small;
+
+	return (
+		// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+		<div className={isClickable ? 'click' : ''} onClick={onClick}>
+			{(!src && icon) && (
+				<div
+					className={asm.joinClasses(s.charContainer, smallClass)}
+					style={{ backgroundColor: color }}
+				>
+					<span className={asm.joinClasses('icon', icon)} />
+				</div>
+			)}
+			{src && <img className={asm.joinClasses(s.img, smallClass)} src={src} alt={alt} />}
+			{!src && !icon
+					&& (
+						<div
+							className={asm.joinClasses(s.charContainer, smallClass)}
+							style={{ backgroundColor: color }}
+						>
+							<h5 className={asm.joinClasses(s.char, 'h5')}>
+								{char}
+							</h5>
+						</div>
+					)}
+		</div>
+	);
+}
