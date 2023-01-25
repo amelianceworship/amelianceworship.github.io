a3774e6cec51ebcabd3464fd41301615ffba46f3
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-25 02:26:21 +0200

    ^ update user  slice - update user actions for updated api

--- src/App/store/user/actions/signIn.ts
+++ src/App/store/user/actions/signIn.ts
@@ -1,16 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { api } from '~api/index';
-import { rejectErrorMessage } from '~store/helpers/rejectErrorMessage';
-import { SignIn } from '~types/api/google/firebase/auth/signIn';
+import { rejectError } from '~store/helpers/rejectError';
+import { returnActionError } from '~store/helpers/returnActionError';
+import type { SignIn } from '~types/api/google/firebase/auth/signIn';
+import type { ErrorString } from '~types/api/google/firebase/commons/ErrorString';
+import type { User } from '~types/api/google/firebase/commons/User';
 
-export const signIn = createAsyncThunk(
+type CreateAsyncThunkReturned = Pick<User, 'uid' | 'displayName' | 'photoURL' | 'email' > | ErrorString;
+type CreateAsyncThunkArguments = SignIn;
+interface CreateAsyncThunkConfig { rejectValue: ErrorString }
+
+export const signIn = createAsyncThunk<
+CreateAsyncThunkReturned, CreateAsyncThunkArguments, CreateAsyncThunkConfig
+>(
 	'user/signIn',
 	async ({ email, password }: SignIn, thunkAPI) => {
 		try {
-			return await api.google.firebase.signIn({ email, password });
+			const response = await api.google.firebase.auth.signIn({ email, password });
+
+			if ('error' in response) return returnActionError(response);
+
+			return {
+				uid: response.user.uid || '',
+				displayName: response.user.displayName || '',
+				photoURL: response.user.photoURL || '',
+				email: response.user.email || '',
+			};
 		} catch (error) {
-			return thunkAPI.rejectWithValue(rejectErrorMessage);
+			return thunkAPI.rejectWithValue(rejectError);
 		}
 	},
 );
--- src/App/store/user/userSlice.ts
+++ src/App/store/user/userSlice.ts
@@ -1,8 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-import { CreateUserResponse } from '~types/api/google/firebase/auth/createUser';
-import { SignInResponse } from '~types/api/google/firebase/auth/signIn';
-import { ErrorResponse } from '~types/api/google/firebase/ErrorResponse';
+import type { ErrorString } from '~types/api/google/firebase/commons/ErrorString';
+import type { User } from '~types/api/google/firebase/commons/User';
 
 import { createUser } from './actions/createUser';
 import { signIn } from './actions/signIn';
@@ -10,18 +10,32 @@ import { signInWithGoogle } from './actions/signInWithGoogle';
 
 interface UserState {
 	isLoading: boolean;
-	error: string;
+	error: unknown;
+	uid: string;
+	displayName: string;
+	photoURL: string;
 	email: string;
-	token: string;
-	id: string;
+	status: 'user' | 'admin' | 'owner';
+	sex: 'male' | 'female' | 'unknown';
+	role: string;
+	lastActiveChatId: string;
+	lastVisitDate: string;
+	isOnline: boolean;
 }
 
 const initialState: UserState = {
 	isLoading: false,
 	error: '',
+	uid: '',
+	displayName: '',
+	photoURL: '',
 	email: '',
-	token: '',
-	id: '',
+	status: 'user',
+	sex: 'unknown',
+	role: '',
+	lastActiveChatId: '',
+	lastVisitDate: '',
+	isOnline: false,
 };
 
 export const userSlice = createSlice({
@@ -29,14 +43,25 @@ export const userSlice = createSlice({
 	initialState,
 	reducers: {
 		setUser(state, action) {
+			state.uid = action.payload.uid;
+			state.displayName = action.payload.displayName;
+			state.photoURL = action.payload.photoURL;
 			state.email = action.payload.email;
-			state.token = action.payload.token;
-			state.id = action.payload.id;
 		},
 		removeUser(state) {
+			state.uid = '';
+			state.displayName = '';
+			state.photoURL = '';
 			state.email = '';
-			state.token = '';
-			state.id = '';
+			state.status = 'user';
+			state.sex = 'unknown';
+			state.role = '';
+			state.lastActiveChatId = '';
+			state.lastVisitDate = '';
+			state.isOnline = false;
+		},
+		resetError(state) {
+			state.error = '';
 		},
 	},
 	extraReducers: (builder) => {
@@ -45,75 +70,100 @@ export const userSlice = createSlice({
 			.addCase(createUser.pending, (state) => {
 				state.isLoading = true;
 				state.error = '';
+				state.uid = '';
+				state.displayName = '';
+				state.photoURL = '';
 				state.email = '';
-				state.token = '';
-				state.id = '';
+				state.status = 'user';
+				state.sex = 'unknown';
+				state.role = '';
+				state.lastActiveChatId = '';
+				state.lastVisitDate = '';
+				state.isOnline = false;
 			})
 			.addCase(
 				createUser.fulfilled,
-				(state, action: PayloadAction<CreateUserResponse | ErrorResponse | unknown>) => {
-					if ((action.payload as ErrorResponse)?.errorMessage) {
-						state.error = (action.payload as ErrorResponse).errorMessage;
+				(state, action: PayloadAction<Pick<User, 'uid' | 'displayName' | 'photoURL' | 'email' > | ErrorString>) => {
+					if ('error' in action.payload) {
+						state.error = action.payload.error;
 					} else {
-						state.email = (action.payload as CreateUserResponse).email;
-						state.id = (action.payload as CreateUserResponse).uid;
-						state.token = (action.payload as CreateUserResponse).accessToken;
+						state.uid = action.payload.uid;
+						state.displayName = action.payload.displayName;
+						state.photoURL = action.payload.photoURL;
+						state.email = action.payload.email;
 					}
+
 					state.isLoading = false;
 				},
 			)
 			.addCase(createUser.rejected, (state, action: PayloadAction<unknown>) => {
-				(state.error as unknown) = action.payload;
+				state.error = (action.payload as ErrorString).error;
 				state.isLoading = false;
 			})
 
 			.addCase(signIn.pending, (state) => {
 				state.isLoading = true;
 				state.error = '';
+				state.uid = '';
+				state.displayName = '';
+				state.photoURL = '';
 				state.email = '';
-				state.token = '';
-				state.id = '';
+				state.status = 'user';
+				state.sex = 'unknown';
+				state.role = '';
+				state.lastActiveChatId = '';
+				state.lastVisitDate = '';
+				state.isOnline = false;
 			})
 			.addCase(
 				signIn.fulfilled,
-				(state, action: PayloadAction<SignInResponse | ErrorResponse | unknown>) => {
-					if ((action.payload as ErrorResponse)?.errorMessage) {
-						state.error = (action.payload as ErrorResponse).errorMessage;
+				(state, action: PayloadAction<Pick<User, 'email' | 'displayName' | 'photoURL' | 'uid' > | ErrorString>) => {
+					if ('error' in action.payload) {
+						state.error = action.payload.error;
 					} else {
-						state.email = (action.payload as SignInResponse).email;
-						state.id = (action.payload as SignInResponse).uid;
-						state.token = (action.payload as SignInResponse).accessToken;
+						state.email = action.payload.email;
+						state.displayName = action.payload.displayName;
+						state.photoURL = action.payload.photoURL;
+						state.uid = action.payload.uid;
 					}
 					state.isLoading = false;
 				},
 			)
 			.addCase(signIn.rejected, (state, action: PayloadAction<unknown>) => {
-				(state.error as unknown) = action.payload;
+				state.error = (action.payload as ErrorString).error;
 				state.isLoading = false;
 			})
 
 			.addCase(signInWithGoogle.pending, (state) => {
 				state.isLoading = true;
 				state.error = '';
+				state.uid = '';
+				state.displayName = '';
+				state.photoURL = '';
 				state.email = '';
-				state.token = '';
-				state.id = '';
+				state.status = 'user';
+				state.sex = 'unknown';
+				state.role = '';
+				state.lastActiveChatId = '';
+				state.lastVisitDate = '';
+				state.isOnline = false;
 			})
 			.addCase(
 				signInWithGoogle.fulfilled,
-				(state, action: PayloadAction<SignInResponse | ErrorResponse | unknown>) => {
-					if ((action.payload as ErrorResponse)?.errorMessage) {
-						state.error = (action.payload as ErrorResponse).errorMessage;
+				(state, action: PayloadAction<Pick<User, 'email' | 'displayName' | 'photoURL' | 'uid' > | ErrorString>) => {
+					if ('error' in action.payload) {
+						state.error = action.payload.error;
 					} else {
-						state.email = (action.payload as SignInResponse).email;
-						state.id = (action.payload as SignInResponse).uid;
-						state.token = (action.payload as SignInResponse).accessToken;
+						state.email = action.payload.email;
+						state.displayName = action.payload.displayName;
+						state.photoURL = action.payload.photoURL;
+						state.uid = action.payload.uid;
 					}
 					state.isLoading = false;
 				},
 			)
 			.addCase(signInWithGoogle.rejected, (state, action: PayloadAction<unknown>) => {
-				(state.error as unknown) = action.payload;
+				state.error = (action.payload as ErrorString).error;
 				state.isLoading = false;
 			});
 	},
