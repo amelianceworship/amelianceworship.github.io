3aff26fefd55800b10fbb8fad0a729e6c6660a4c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-01-28 16:16:14 +0200

    ^ update songs list slice

--- src/App/store/songsList/actions/fetchSongsList.ts
+++ src/App/store/songsList/actions/fetchSongsList.ts
@@ -4,37 +4,49 @@ import asm from 'asm-ts-scripts';
 import { api } from '~api/index';
 import { TABLE_NAMES } from '~app/constants/TABLE_NAMES';
 import { GOOGLE_SPREADSHEETS_IDS } from '~constants/GOOGLE_SPREADSHEETS_IDS';
-import { rejectErrorMessage } from '~store/helpers/rejectErrorMessage';
+import { rejectError } from '~store/helpers/rejectError';
+import type { ErrorString } from '~types/api/google/firebase/commons/ErrorString';
 
-function prepareData(data: { position: string; value: string }[] | null) {
+// TODO: add export types to library
+
+type ObjItem = Record<string, string | number>;
+type GroupOfObj = [string, ObjItem[]];
+type GroupBy = GroupOfObj[];
+
+function prepareData(data: { position: string; value: string }[] | null): GroupBy {
 	let combinedListOfData;
 	if (data) {
 		const listOfDataSorted = asm.sortArrayLocalCompare(data, 'value');
 		const listOfDataCleaned = asm.removeEmptyValues(listOfDataSorted, 'value');
 		combinedListOfData = asm.groupBy(listOfDataCleaned, 'value');
 	}
-	return combinedListOfData;
+	return combinedListOfData as GroupBy;
 }
+export type SongsListData = Partial<Record<string, GroupBy>>;
+export type CreateAsyncThunkReturned = SongsListData | ErrorString;
+type CreateAsyncThunkArguments = void;
+interface CreateAsyncThunkConfig { rejectValue: ErrorString }
 
-export const fetchSongsList = createAsyncThunk(
+export const fetchSongsList = createAsyncThunk<
+CreateAsyncThunkReturned, CreateAsyncThunkArguments, CreateAsyncThunkConfig
+>(
 	'songsList/fetchSongsList',
 	async (_, thunkAPI) => {
-
 		try {
 			const response = await api.google.appsscript.getAllTitledColumnsDataSingle({
-				spreadsheetId: GOOGLE_SPREADSHEETS_IDS.songslist,
+				spreadsheetId: GOOGLE_SPREADSHEETS_IDS.SONGSLIST,
 				sheetName: 'common',
 			});
 
 			return {
-				[TABLE_NAMES.general]: prepareData(response.data.general.values),
-				[TABLE_NAMES.study]: prepareData(response.data.study.values),
-				[TABLE_NAMES.christmas]: prepareData(response.data.christmas.values),
-				[TABLE_NAMES.easter]: prepareData(response.data.easter.values),
-				[TABLE_NAMES.defer]: prepareData(response.data.defer.values),
+				[TABLE_NAMES.GENERAL]: prepareData(response.data.general.values),
+				[TABLE_NAMES.STUDY]: prepareData(response.data.study.values),
+				[TABLE_NAMES.CHRISTMAS]: prepareData(response.data.christmas.values),
+				[TABLE_NAMES.EASTER]: prepareData(response.data.easter.values),
+				[TABLE_NAMES.DEFER]: prepareData(response.data.defer.values),
 			};
 		} catch (error) {
-			return thunkAPI.rejectWithValue(rejectErrorMessage);
+			return thunkAPI.rejectWithValue(rejectError);
 		}
 	},
 );
--- src/App/store/songsList/songsListSlice.ts
+++ src/App/store/songsList/songsListSlice.ts
@@ -1,58 +1,59 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 import { TABLE_NAMES } from '~app/constants/TABLE_NAMES';
+import type { ErrorString } from '~types/api/google/firebase/commons/ErrorString';
 
+import type { SongsListData } from './actions/fetchSongsList';
 import { fetchSongsList } from './actions/fetchSongsList';
 
-interface SongsList {
-	[key: string]: [string, { position: string; value: string }[]][];
-}
+// type SongsList = Record<string, [string, { position: string; value: string }[]][]>;
 
 interface SongsListSlice {
 	isLoading: boolean;
 	error: string;
-	songsList: SongsList;
+	songsList: SongsListData;
 	songsTableNames: string[];
 }
 
 const initSongsListSlice: SongsListSlice = {
 	isLoading: false,
 	error: '',
-	songsList: {} as SongsList,
+	songsList: {} as SongsListData,
 	songsTableNames: [
-		TABLE_NAMES.general,
-		TABLE_NAMES.study,
-		TABLE_NAMES.christmas,
-		TABLE_NAMES.easter,
-		TABLE_NAMES.defer,
+		TABLE_NAMES.GENERAL,
+		TABLE_NAMES.STUDY,
+		TABLE_NAMES.CHRISTMAS,
+		TABLE_NAMES.EASTER,
+		TABLE_NAMES.DEFER,
 	],
 };
 
 export const songsListSlice = createSlice({
 	name: 'songsList',
 	initialState: initSongsListSlice,
-	reducers: {
-	},
+	reducers: {},
 	extraReducers: (builder) => {
 		builder
 			.addCase(fetchSongsList.pending, (state) => {
 				state.isLoading = true;
 				state.error = '';
-				state.songsList = {} as SongsList;
+				state.songsList = {} as SongsListData;
 			})
 			.addCase(
 				fetchSongsList.fulfilled,
-				(state, action: PayloadAction<SongsList | unknown>) => {
-					if (action.payload && typeof action.payload === 'object') {
-						state.songsList = action?.payload as SongsList;
+				(state, action: PayloadAction<SongsListData | ErrorString>) => {
+					if ('error' in action.payload) {
+						state.error = (action.payload as ErrorString).error;
+					} else {
+						state.songsList = action.payload;
 					}
 					state.isLoading = false;
 				},
 			)
 			.addCase(fetchSongsList.rejected, (state, action: PayloadAction<unknown>) => {
-				(state.error as unknown) = action.payload;
+				state.error = (action.payload as ErrorString).error;
 				state.isLoading = false;
 			});
-
 	},
 });
