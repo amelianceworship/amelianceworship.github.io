fef5d890e3bf648f25a6eed5161afcfaa061056c
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-13 12:49:04 +0200

    * move About page to folder

---
+++ src/App/pages/About/About.tsx
@@ -0,0 +1,12 @@
+import { Main } from '~/asmlib/components/blocks/Main';
+import { Typography } from '~/asmlib/components/Typography';
+
+export function About() {
+	return (
+		<Main>
+			<Typography component="h1">
+				About
+			</Typography>
+		</Main>
+	);
+}
