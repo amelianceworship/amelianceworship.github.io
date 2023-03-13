daf6c0233ce1f6fab2dfcf445534892de4f6a749
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-13 15:15:52 +0200

    ^ update  screen sizes

--- src/App/hooks/useScreenQuery.ts
+++ src/App/hooks/useScreenQuery.ts
@@ -4,11 +4,12 @@ import { SCREEN_SIZES } from '~constants/SCREEN_SIZES';
 
 export function useScreenQuery() {
 	return {
-		isScreenXX: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XX}px)` }),
-		isScreenXL: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XL}px)` }),
-		isScreenLG: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_LG}px)` }),
-		isScreenMD: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_MD}px)` }),
-		isScreenXS: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_XS}px)` }),
-		isScreenSS: useMediaQuery({ query: `(max-width: ${SCREEN_SIZES.SCREEN_SS}px)` }),
+		isScreenXX: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XX}px)` }),
+		isScreenXL: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XL}px)` }),
+		isScreenLG: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_LG}px)` }),
+		isScreenMD: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_MD}px)` }),
+		isScreenSM: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_SM}px)` }),
+		isScreenXS: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_XS}px)` }),
+		isScreenSS: useMediaQuery({ query: `(min-width: ${SCREEN_SIZES.SCREEN_SS}px)` }),
 	};
 }
