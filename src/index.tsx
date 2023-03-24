76d407f1f246b0ed86e981412194e922fced14f3
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-03-24 23:55:14 +0200

    * fix font imports

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { App } from '~app/App';
 
-import '@fontsource/montserrat/300.css';
-import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
 import '@fontsource/montserrat/700.css';
 
 import './index.scss';
