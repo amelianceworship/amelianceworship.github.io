784cfe0577a4c7cccde0e9dda880f60001d8209d
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-01 23:49:07 +0300

    * update songsListSlice • refactor fetchSonsList logic • add some field and actions

--- src/App/store/helpers/rejectError.ts
+++
@@ -1,5 +0,0 @@
-import type { ErrorString } from '~types/api/google/firebase/commons/ErrorString';
-
-export const rejectError: ErrorString = {
-	error: 'Сорі, якась невідома помилка! ¯\\_O_o_/¯',
-};
