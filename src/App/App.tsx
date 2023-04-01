27d97cde16e382d4b13dc54b1e95b6d0408c11b1
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-01 23:39:49 +0300

    ^ add ToastProvider

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,12 +5,16 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { AppRouter } from '~components/AppRouter/AppRouter';
 import { persistor, store } from '~store/store';
 
+import { ToastBarProvider } from '~/ameliance-ui/components/_LAB/toastbar';
+
 export function App() {
 	return (
 		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<AppRouter />
-			</PersistGate>
+			<ToastBarProvider maxToast={5}>
+				<PersistGate loading={null} persistor={persistor}>
+					<AppRouter />
+				</PersistGate>
+			</ToastBarProvider>
 		</Provider>
 	);
 }
