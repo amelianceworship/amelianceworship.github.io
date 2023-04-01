571bb643867b56d986065a9af5c8ca9dc1df0694
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-01 23:45:55 +0300

    + add reload songsLost and reset settings to Settings

--- src/App/components/Layout/Header/Settings.tsx
+++ src/App/components/Layout/Header/Settings.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 
+import { clearLocalStorageAndReload } from '~helpers/clearLocalStorageAndReload';
 import { appSlice } from '~store/app/appSlice';
 import { useTypedDispatch } from '~store/hooks/useTypedDispatch';
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
+import { fetchSongsList } from '~store/songsList/actions/fetchSongsList';
 
 import { Button } from '~/ameliance-ui/components/Button';
-import { SettingsIcon } from '~/ameliance-ui/components/icons/SettingsIcon';
-import { Menu, MenuContainer, MenuItem } from '~/ameliance-ui/components/Menu';
+import { Icon } from '~/ameliance-ui/components/Icon';
+import { MoonIcon } from '~/ameliance-ui/components/icons/MoonIcon';
+import { MoreVerticalIcon } from '~/ameliance-ui/components/icons/MoreVerticalIcon';
+import { RefreshCcwIcon } from '~/ameliance-ui/components/icons/RefreshCcwIcon';
+import { RotateCcwIcon } from '~/ameliance-ui/components/icons/RotateCcwIcon';
+import { SunIcon } from '~/ameliance-ui/components/icons/SunIcon';
+import {
+	Menu, MenuContainer, MenuDivider, MenuItem,
+} from '~/ameliance-ui/components/Menu';
 import { Typography } from '~/ameliance-ui/components/Typography';
 import { toggleTheme } from '~/ameliance-ui/scripts/toggleTheme';
 
@@ -25,6 +34,16 @@ export function Settings() {
 		setIsMenuOpen(false);
 	};
 
+	const handleResetMenuItemOnClick = () => {
+		setIsMenuOpen(false);
+		clearLocalStorageAndReload();
+	};
+
+	const handleReloadSongsLIstMenuItemOnClick = () => {
+		setIsMenuOpen(false);
+		dispatch(fetchSongsList());
+	};
+
 	const handleThemeMenuItemOnClick = () => {
 		const newTheme = toggleTheme();
 		dispatch(actions.setTheme(newTheme));
@@ -39,7 +58,22 @@ export function Settings() {
 				menuOrigin={{ horizontal: 'right', vertical: 'top' }}
 				preventItemClickClose
 			>
+				<MenuItem onClick={handleResetMenuItemOnClick}>
+					<Icon><RotateCcwIcon /></Icon>
+					<Typography component="p1">
+						Скинути налаштування
+					</Typography>
+				</MenuItem>
+				<MenuDivider />
+				<MenuItem onClick={handleReloadSongsLIstMenuItemOnClick}>
+					<Icon><RefreshCcwIcon /></Icon>
+					<Typography component="p1">
+						Оновити список пісень
+					</Typography>
+				</MenuItem>
+				<MenuDivider />
 				<MenuItem onClick={handleThemeMenuItemOnClick}>
+					<Icon>{theme === 'dark' ? <MoonIcon /> : <SunIcon />}</Icon>
 					<Typography component="p1">
 						Тема:
 						{' '}
@@ -48,7 +82,7 @@ export function Settings() {
 				</MenuItem>
 			</Menu>
 			<Button type="text" onClick={handelIconMenuClick}>
-				<SettingsIcon />
+				<MoreVerticalIcon />
 			</Button>
 		</MenuContainer>
 	);
