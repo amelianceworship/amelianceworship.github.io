6b5f32a7cb0d5007e1974c194537a39063e25911
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-01 23:41:45 +0300

    + extends ErrorPage with PageNotFound

--- src/App/pages/ErrorPage/ErrorPage.tsx
+++ src/App/pages/ErrorPage/ErrorPage.tsx
@@ -7,44 +7,37 @@ import { Button } from '~/ameliance-ui/components/Button';
 import { Grid } from '~/ameliance-ui/components/Grid';
 import { Typography } from '~/ameliance-ui/components/Typography';
 
+import { PageNotFound } from './PageNotFound/PageNotFound';
+
 import s from './ErrorPage.module.scss';
 
 export function ErrorPage() {
 	const error = useRouteError();
 	const navigate = useNavigate();
+	if (isRouteErrorResponse(error) && error.status) return <PageNotFound />;
 	return (
 		<Block component="main">
 			<Grid container component="section" className={s.container}>
-				<Button size="small" onClick={() => navigate(ROUTES.home)}>
-					На головну
-				</Button>
-				<Block className={s.title}>
-					<Typography component="p1" display="h1" className={s.title40}>40</Typography>
-					<Typography component="p1" display="h1" className={s.title4}>4</Typography>
-				</Block>
-				<Block className={s.description}>
-					<>
-						{error && (
-							<>
-								<Typography component="h5">
-									{isRouteErrorResponse(error) && error.status}
-								</Typography>
-								<Typography component="h6">
-									{error instanceof Error && error.message}
-									{isRouteErrorResponse(error) && error.statusText}
-									{isRouteErrorResponse(error) && error.data.message}
-								</Typography>
-							</>
-						)}
-						<Typography component="p2">
-							Вибачте, сторінки не існує
-							<br />
-							або функціонал ще в процесі розробки
-							<br />
-							¯\_(ツ)_/¯
-						</Typography>
-					</>
-				</Block>
+				<>
+					<Button size="small" onClick={() => navigate(ROUTES.home)}>
+						На головну
+					</Button>
+					<Typography component="h3">
+						Сорі, виникла помилка =(
+					</Typography>
+					<Block className={s.description}>
+						<>
+							<Typography component="h4">
+								{isRouteErrorResponse(error) && error.status}
+							</Typography>
+							<Typography component="h5">
+								{error instanceof Error && error.message}
+								{isRouteErrorResponse(error) && error.statusText}
+								{isRouteErrorResponse(error) && error.data.message}
+							</Typography>
+						</>
+					</Block>
+				</>
 			</Grid>
 		</Block>
 	);
