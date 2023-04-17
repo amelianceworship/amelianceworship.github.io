b93430360dba3212fbbc71567990bc225c5e91fc
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-17 15:35:46 +0300

    * fix import returnError

--- src/App/helpers/returnError.ts
+++ src/App/helpers/returnError.ts
@@ -1,6 +1,6 @@
-import a from '~/ameliance-scripts';
+import { returnError as returnErr } from '~/ameliance-scripts';
 import { APP } from '~constants/APP';
 
 export function returnError(error: unknown): string {
-	return a.lab.returnError(error, APP.name);
+	return returnErr(error, APP.name);
 }
