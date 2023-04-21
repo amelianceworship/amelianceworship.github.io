5012721254e5bbfd7e710f4e27dd71edd99edee5
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-21 19:39:44 +0300

    * add isFillProfile to useAuth

--- src/App/hooks/useAuth.ts
+++ src/App/hooks/useAuth.ts
@@ -1,11 +1,12 @@
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
 
 export function useAuth() {
-	const { displayName, status } = useTypedSelector((state) => state.userReducer);
+	const { email, status, sex } = useTypedSelector((state) => state.userReducer);
 	return {
-		isAuth: !!displayName,
+		isAuth: !!email,
 		isOwner: status === 'owner',
 		isAdmin: status === 'admin',
 		isUser: status === 'user',
+		isFillProfile: sex,
 	};
 }
