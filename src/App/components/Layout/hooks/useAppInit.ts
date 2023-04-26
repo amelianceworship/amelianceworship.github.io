aff63763b74660b9abba467ebecbdd4ed3c2e224
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-04-26 18:56:52 +0300

    * refactor useAppInit hook

--- src/App/components/Layout/hooks/useAppInit.ts
+++ src/App/components/Layout/hooks/useAppInit.ts
@@ -1,15 +1,12 @@
 import { useEffect, useLayoutEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { onAuthStateChanged } from 'firebase/auth';
-
-import { auth } from '~api/google/firebase/firebase';
-import { api } from '~api/index';
+import { PRIVATE_ROUTES, ROUTES } from '~constants/ROUTES';
 import { useViewportHeight } from '~hooks/useViewportHeight';
 import { useTypedDispatch } from '~store/hooks/useTypedDispatch';
 import { useTypedSelector } from '~store/hooks/useTypedSelector';
-import { updateUser } from '~store/user/actions/updateUser';
-import { userSlice } from '~store/user/userSlice';
+import { getAuthUserId } from '~store/user/actions/getAuthUserId';
+import { initUserOnPageLoad } from '~store/user/actions/initUserOnPageLoad';
 
 import { useInitTheme } from '~/ameliance-ui/hooks/useInitTheme';
 
@@ -19,53 +16,43 @@ export function useAppInit() {
 	useViewportHeight();
 
 	const { theme } = useTypedSelector((state) => state.appReducer);
+	const { user, authUserId } = useTypedSelector((state) => state.userReducer);
+	const { uid } = user;
 
 	// *----- set or init theme -----
 	useInitTheme(theme);
 
 	const dispatch = useTypedDispatch();
-	const { actions } = userSlice;
 
 	const navigate = useNavigate();
 	const location = useLocation();
-	const [startLocation, setStartLocation] = useState('');
+	const [startLocation, setStartLocation] = useState<string | null>(null);
 
 	useLayoutEffect(() => {
 		document.body.classList.add('scroll');
+		dispatch(getAuthUserId());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	// *----- set path init browser path -----
 	useLayoutEffect(() => {
-		setStartLocation(location.pathname);
+		const locationToNavigate = location.pathname === ROUTES.login
+			|| location.pathname === ROUTES.signup ? PRIVATE_ROUTES.users : location.pathname;
+		setStartLocation(locationToNavigate || ROUTES.home);
 	}, [location]);
 
+	useLayoutEffect(() => {
+		if (authUserId) dispatch(initUserOnPageLoad({ uid: authUserId }));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [authUserId]);
+
 	useEffect(() => {
-		const initFetch = async () => {
-			onAuthStateChanged(auth, async (user) => {
-				if (user) {
-					const userFromDatabase = await api.google.firebase.database.users
-						.getUserById({ userId: user.uid });
-
-					dispatch(updateUser({
-						uid: user.uid,
-						visitsCount: (userFromDatabase.user.visitsCount || 0) + 1,
-					}));
-
-					const userFinalFromDatabase = await api.google.firebase.database.users
-						.getUserById({ userId: user.uid });
-
-					dispatch(actions.setUser({
-						...userFinalFromDatabase.user,
-					}));
-				}
-				if (!isInit) {
-					if (startLocation) navigate(startLocation);
-					setIsInit(true);
-				}
-			});
-		};
-		initFetch();
+		if (uid || uid === null) {
+			if (startLocation) navigate(startLocation);
+			setIsInit(true);
+		}
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [uid]);
 
 	return { isInit };
 }
