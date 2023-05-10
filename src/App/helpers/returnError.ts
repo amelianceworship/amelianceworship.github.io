067a60cd9cb1769f69f5c7e0453a9949e074370b
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2023-05-10 22:53:33 +0300

    * update helper returnError to ameliance scripts

--- src/App/helpers/returnError.ts
+++ src/App/helpers/returnError.ts
@@ -2,5 +2,5 @@ import { returnError as returnErr } from '~/ameliance-scripts';
 import { APP } from '~constants/APP';
 
 export function returnError(error: unknown): string {
-	return returnErr(error, APP.name, true);
+	return returnErr(error, APP.name, 1);
 }
